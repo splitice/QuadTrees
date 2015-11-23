545b53ddea1a793cf68fb1b0b3866427c5c55792
splitice <mat999@gmail.com>
2015-11-23 15:34:13 +1100

    commit some simple wrappers for both quadtree types

---
+++ QuadTrees/Wrappers/QuadTreePointWrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuadTrees.QTreePoint;
+
+namespace QuadTrees.Wrappers
+{
+    /// <summary>
+    /// A simple container for a point in a QuadTree
+    /// </summary>
+    public class QuadTreePointWrapper: IPointQuadStorable
+    {
+        private PointF _point;
+
+        public PointF Point
+        {
+            get { return _point; }
+        }
+
+        public QuadTreePointWrapper(PointF point)
+        {
+            _point = point;
+        }
+    }
+}
---
+++ QuadTrees/Wrappers/QuadTreeRectWrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuadTrees.QTreeRect;
+
+namespace QuadTrees.Wrappers
+{
+    /// <summary>
+    /// A simple container for a rectangle in a QuadTree
+    /// </summary>
+    public class QuadTreeRectWrapper : IRectQuadStorable
+    {
+        private RectangleF _rect;
+
+        public RectangleF Rect
+        {
+            get { return _rect; }
+        }
+
+        public QuadTreeRectWrapper(RectangleF rect)
+        {
+            _rect = rect;
+        }
+    }
+}
